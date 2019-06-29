39c2c366b48387d1c954b2da2c32f64ce60cdd35
Bin Yang <binbinxm@gmail.com>
2019-06-29 09:24:25 +0000

    ip updated

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class ip_table(object):
         self.description = desc
         self.mac = mac
 
-arduino = ip_table('arduino as iot hub', '60:01:94:0a:a1:fe', '192.168.0.104')
+arduino = ip_table('arduino as iot hub', '60:01:94:0a:a1:fe', '192.168.1.202')
 sp2_00 = ip_table('sp2 for unknown controller', '34:ea:34:b8:79:a0')
 
